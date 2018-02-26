4f402f0c0e7663d99a39d199eec466409a8dfa82
Janch <janchestrada@gmail.com>
2018-02-27 04:11:51 +0800

    Updated bg health

--- wp-content/themes/store/header.php
+++ wp-content/themes/store/header.php
@@ -8,48 +8,53 @@
  */
 ?>
 <?php get_template_part('modules/header/head') ?>
-<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Raleway:800i" rel="stylesheet">
+
 <style type="text/css">
 	.label-home{
-		font-family: 'Pacifico', cursive;
-		font-size: 50px;
+		font-family: 'Raleway', sans-serif;
+		font-size: 45px;
 		color: white;
 		position: absolute;
 	    top: 40%;
 	    left: 50%;
 	    transform: translate(-50%, -50%);
+	    text-shadow: 0px 0px 30px black;
 	}
 	.native-tapioca{
 		background-image: url("NativeTapioca.jpg");
 		background-size: cover;
-		min-height: 550px
+		min-height: 575px
 	}
 	.modified-tapioca{
 		background-image: url("ModifiedTapioca.jpg");
 		background-size: cover;
-		min-height: 550px
+		min-height: 575px
 	}
 	.btn {
 	  background-color: transparent;
-	  border: 2px solid black;
-	  color: black;
-	  padding: 14px 28px;
+	  border: 2px solid white;
+	  color: white;
+	  padding: 8px 40px;
 	  cursor: pointer;
-	  text-align: center;
+  	  text-align: center;
 	  font-size: 20px;
+	  font-family: 'Roboto Condensed', sans-serif;
 	  margin: 4px 2px;
 	  transition: 0.3s;
 	  position: absolute;
-	  top: 50%;
+	  top: 55%;
 	  left: 50%;
 	  transform: translate(-50%, -50%);
 	}
 	.default{
-	  border-color: #e7e7e7;
-	  color: black;
+	  border-color: white;
+	  color: white;
 	}
 	.default:hover {
-	    background-color: #e7e7e7;
+	    background-color: white;
+	    border-color: white;
 	}
 </style>
 <body <?php body_class(); ?>>
@@ -65,24 +70,36 @@
         </a>
     </div> -->
 
-    <div class="container-fluid" style="margin-bottom: -30px; background-color: white">
+    <div class="container-fluid" style="background-color: white">
     	<div class="row">
-    		<div class="col-md-6" style="padding: 0px; min-height: 550px; background-color: #954244; margin-right: 1.25px;">    			
+    		<div class="col-md-6" style="padding: 0px; min-height: 578px; background-color: #d54324">    			
     			<div class="native-tapioca">
 	    			<center class="label-home">Native Tapioca</center>
 					<button class="btn default">EXPLORE</button>
-
     			</div>
-    			<!-- <img src="NativeTapioca.jpg" class="img-fluid" alt="Native Tapioca" style="height: 550px; width: 100%; margin-left: -15px"> -->
-
     		</div>
-    		<div class="col-md-6" style="padding: 0px; margin-right: -49px; min-height: 550px; background-color: #842511; margin-left: 1.25px">
+
+    		<div class="col-md-6" style="padding: 0px; min-height: 578px; background-color: #d54324">
     			<div class="modified-tapioca">
 					<center class="label-home">Modified Tapioca</center>
+					<button class="btn default">EXPLORE</button>
 				</div>
 			</div>
     	</div>
 	</div>
+
+    <div class="container-fluid" style="background-color: black; min-height: 225px">
+		<div class="row">
+			<div class="col-md-4" style="min-height: 225px; background-color: #9bd220">
+			</div>
+			<div class="col-md-4" style="min-height: 225px; background-color: #9bd255">
+			</div>
+			<div class="col-md-4" style="min-height: 225px; background-color: #9bd220">
+			</div>
+		</div>
+    </div>
+
+
 	<div class="mega-container">
 		<?php //get_template_part('framework/featured-components/slider', 'swiper'); ?>
 		<?php if (class_exists('woocommerce')) : ?>	
